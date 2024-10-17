5f8c86296ee1cee7307524ffd702092d7603cb80
elli kiiski <kiiski.elli@gmail.com>
2024-10-17 18:04:45 +0300

    tehtävä 3.1

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "id": "1",
+        "name": "elppa koo",
+        "number": "666"
+    },
+    {
+        "id": "2",
+        "name": "jerppa pee",
+        "number": "381"
+    },
+    {
+        "id": "3",
+        "name": "jenppa vee",
+        "number": "123"
+    },
+    {
+        "id": "4",
+        "name": "erppa rää",
+        "number": "345"
+    },
+    {
+        "id": "5",
+        "name": "irppa lää",
+        "number": "222"
+    },
+    {
+        "id": "6",
+        "name": "pimppi vee",
+        "number": "112"
+    },
+    {
+        "id": "7",
+        "name": "viippa wee",
+        "number": "365"
+    },
+    {
+        "id": "8",
+        "name": "eppa pee",
+        "number": "987"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h2>Puhelinluettelon bäkkäri, tervetuloa!</h2>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`)
+})
+
+
