46a1807811ca1ebc8094cb94dd0c64f3229445dd
elli kiiski <kiiski.elli@gmail.com>
2024-11-16 15:31:00 +0200

    tehtävä 3.17

--- index.js
+++ index.js
@@ -102,6 +102,21 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const contact = {
+        name: body.name,
+        number: body.number
+    }
+
+    Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+        .then(updatedContact => {
+            response.json(updatedContact)
+        })
+        .catch(error => next(error))
+})
+
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
